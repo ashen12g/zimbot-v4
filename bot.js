b382d5c8a7a2669281b7990836eb9ee01d3ada68
ashen12g <108817875+ashen12g@users.noreply.github.com>
2022-07-29 20:20:52 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,13 +37,13 @@ global.owner = ['27634090203',''] //owner number +27634090203
 global.wame = ['27634090203'] 
 global.pemilik = ['27634090203'] //owner number +27634090203
 global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
+global.pengguna = '᳆⃞ʀᴇͥᴀͣʟ⃬ͫ🕊️⃕සුද̷්දා"හ්᜴' //username
+global.footer = '᳆⃞ʀᴇͥᴀͣʟ⃬ͫ🕊️⃕සුද̷්දා"හ්᜴'
 global.botnma = 'ZIM BOT INC' //bot name
 global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.ownernma = '᳆⃞ʀᴇͥᴀͣʟ⃬ͫ🕊️⃕සුද̷්දා"හ්᜴' //owner name
+global.packname = '᳆⃞ʀᴇͥᴀͣʟ⃬ͫ🕊️⃕සුද̷්දා"හ්᜴' //sticker package name
+global.author = '᳆⃞ʀᴇͥᴀͣʟ⃬ͫ🕊️⃕සුද̷්දා"හ්᜴' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
@@ -72,7 +72,7 @@ global.f5 = 'application/pdf'
 global.f6 = 'application/vnd.android.package-archive'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
 global.mess = {
-    success: 'Takaenda Done!',
+    success: 'Done!',
     admin: '*This feature is only for admin!*',
     botAdmin: '*zim bot  must be admin first!*',
     owner: '*this feature is only for zimbot owner*',
